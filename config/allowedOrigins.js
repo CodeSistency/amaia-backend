b073993914ba355493a5ae785766f52c0e56eeb0
Jose Perez <valero.jozee@gmail.com>
2023-08-20 16:09:18 -0400

    fixing cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://surtimas.vercel.app',
+    'https://amaia.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
